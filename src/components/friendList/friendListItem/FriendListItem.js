a68311e676fe319fdc72e6a1ccc93c95f53111ae
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2022-12-01 15:09:35 +0200

    не працює рендер <span> у 4  завданні - не сприймає змінну-масив класів

--- src/components/friendList/friendListItem/FriendListItem.js
+++ src/components/friendList/friendListItem/FriendListItem.js
@@ -1,19 +1,25 @@
 import PropTypes from "prop-types";
+// import clsx from "clsx";
 import css from "./FriendListItem.module.css";
 
 
 export const FriendListItem = ({ friendData: {avatar, name, isOnline}}) => {
+    const spanClassNames = ["css.status"];
+    if (isOnline) spanClassNames.push("css.isOnline");
+
     return  <li className={css.item}>
-                <span className={css.status}
-                
-                /* isOnline ???? */
-                ></span>
+                <span 
+                className={spanClassNames.join(" ")}
+                >
+                </span>
+
                 <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
                 <p className={css.name}>{name}</p>
             </li>
 }
 
 FriendListItem.propTypes = {
+    friendData: PropTypes.func,
     avatar: PropTypes.string,
     isOnline: PropTypes.bool,
     name: PropTypes.string
